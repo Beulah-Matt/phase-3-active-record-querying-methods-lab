4afd890c61fce8ec42f20208255e384a7a464b11
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-01 16:45:45 +0300

    commit tree add23d59ac5d752e244cf535ead44a5fd0c8957d

---
+++ app/models/show.rb
@@ -0,0 +1,10 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        Show.where(":name".maximum(:rating))
+    end
+end
